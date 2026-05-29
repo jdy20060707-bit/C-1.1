254d462a766b1b0e595f78abc31cfc92602f4802
jdy20060707-bit <jdy20060707@gmail.com>
2026-05-29 18:58:46 +0900

    26.05.29
    hello C!
    첫 C.

---
+++ hello c!.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+	printf("hello c!");
+	
+	return 0;
+}
---
+++ hello c!_줄바꿈.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+
+{
+
+	printf("hello c!\n");
+	printf("from chulSoo\n");
+	
+	return 0;
+}
